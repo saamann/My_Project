22ac716c77d50c98b98671d01c143b4dc8131422
saamann <mark428888@gmail.com>
2026-01-18 20:47:14 +0530

    Initial commit

--- frontend-projects-2/project-10_404_error_page/js/index.js
+++
@@ -1,5 +0,0 @@
-//For Go Back Button : (It's Optional You Can Also Use Anchor Tag Of Html)
-let btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-  window.location.href="/"
-});
--- frontend-projects-2/project-2_calculator/js/index.js
+++
@@ -1,7 +0,0 @@
-      const buttons = document.querySelectorAll("#btn");
-      const display = document.getElementById("display");
-      buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-          display.value += button.value;
-        });
-      });
